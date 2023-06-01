39d1e525ae132a0d532845534f8cf8233ce608a4
044.2023dounyu <2023dounyukenshu.044@gmail.com>
2023-06-01 15:34:06 +0900

    導入研修

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -33,7 +33,7 @@ public class NewServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // おまじないとしてのインスタンスを生成
-        request.setAttribute("message", new Task());
+        request.setAttribute("task", new Task());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
